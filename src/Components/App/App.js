0d0e029c4488a7f753f89ee497be7f2d559b692a
Liang <lianginchina1999@gmail.com>
2024-01-20 13:38:53 +0800

    remove unused effect hook

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React,{useState,useEffect} from 'react';
+import React,{useState} from 'react';
 import SearchBar from '../SearchBar/SearchBar';
 
 import SearchResult from '../SearchResult/SearchResult';
